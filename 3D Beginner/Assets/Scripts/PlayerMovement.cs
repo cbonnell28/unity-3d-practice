8555dea52ad14088bab7a45f16d2662ff83ae636
Christian Alexander Bonnell <cbonnell28@gmail.com>
2019-08-31 21:35:37 -0400

    Added player control and animation
    Continued process of learning animation and add a player movement script that changes the state of the animator to allow transition between a walking and idle state of the character.

---
+++ 3D Beginner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Ridigbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Ridigbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Ridigbody.MovePosition(m_Ridigbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Ridigbody.MoveRotation(m_Rotation);
+    }
+}
